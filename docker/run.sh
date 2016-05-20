84cd86a0d45bad8952035ed13183374d02224379
UnknownHero <unknownpostie@gmail.com>
2016-05-21 01:53:01 +0300

    Closes #10 add default CONTEXT name customizing

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-CONTEXT_NAME="default"
+source ./default.sh;
+
+CONTEXT_NAME=$DEFAULT_CONTEXT_NAME;
 
 if [ $# -eq 1 ]
 then
@@ -12,7 +14,8 @@ if ! [ -d "contexts/$CONTEXT_NAME" ]; then
     exit 1;
 fi
 
-cd contexts/$CONTEXT_NAME && docker-compose  build && docker-compose up -d && cd ..
+cd contexts/$CONTEXT_NAME && docker-compose pull --ignore-pull-failures && docker-compose  build && docker-compose up -d && cd ..
+
 
 
 
